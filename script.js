e40f197ebebd2bced6951e3807e66fed6bce7feb
Mukesh-SE <mukeshodhano2018@gmail.com>
2023-10-20 19:28:37 +0500

    close btn

--- script.js
+++ script.js
@@ -3,22 +3,29 @@ const cards = container.querySelectorAll(".card");
 
 // horizontal Scrolling
 container.addEventListener("wheel", (e) => {
-	if (e.deltaY > 0) {
-		// Scroll right
-		container.scrollLeft += 300; // Adjust the scroll amount as needed
+  if (e.deltaY > 0) {
+    // Scroll right
+    container.scrollLeft += 300; // Adjust the scroll amount as needed
+  } else {
+    // Scroll left
+    container.scrollLeft -= 300; // Adjust the scroll amount as needed
+  }
 
-	} else {
-		// Scroll left
-		container.scrollLeft -= 300; // Adjust the scroll amount as needed
-	}
-
-	// Prevent the default behavior of the mouse wheel
-	e.preventDefault();
+  // Prevent the default behavior of the mouse wheel
+  e.preventDefault();
 });
 
 cards.forEach((card) => {
-	card.addEventListener("click", () => {
-		container.classList.toggle("active-container");
-		card.classList.toggle("active");
-	});
+  const close_btn = card.querySelector(".fa-xmark");
+	const prof_pic = card.querySelector('.prof-pic')
+
+  prof_pic.addEventListener("click", () => {
+    container.classList.add("active-container");
+	  card.classList.add("active");
+  });
+
+  close_btn.addEventListener("click", () => {
+    container.classList.remove("active-container");
+    card.classList.remove("active");
+  });
 });
